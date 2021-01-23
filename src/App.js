11ef10e4ef102019de66d5f1d0ee319c3f503e52
Victor Uland <53414263+vuduland@users.noreply.github.com>
2021-01-23 11:37:49 -0600

    header component with default props and proptypes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 }
