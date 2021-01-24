76eaccb5e8942604fe4b023f008a8dd340a21bc7
Victor Uland <53414263+vuduland@users.noreply.github.com>
2021-01-24 11:29:14 -0600

    add onClick function to Header.js, activated when Button.js component is clicked

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,19 @@
 import PropTypes from "prop-types";
 
-const Button = ({ color, text }) => {
+const Button = ({ color, text, onClick }) => {
+  //   const onClick = (e) => {
+  //pass in event object and log it when clicked
+  //console.log(e);
+  //or just log 'click' when clicked
+  //console.log("click");
+  //   };
+
   return (
-    <button style={{ backgroundColor: color }} className="btn">
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className="btn"
+    >
       {text}
     </button>
   );
@@ -11,13 +22,14 @@ const Button = ({ color, text }) => {
 // falls back to these defaults if not specified inline when invoked
 
 Button.defaultProps = {
-  color: "steelblue",
+  color: "steelblue ",
   text: "Add",
 };
 
 Button.propTypes = {
   color: PropTypes.string,
   text: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
