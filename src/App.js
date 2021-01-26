3e6a7844f060f85f5121f355c4009e85bd3ea531
Victor Uland <53414263+vuduland@users.noreply.github.com>
2021-01-25 20:45:38 -0600

    add toggleReminder func to App.js and pass to Task.js to console.log id when x is clicked

--- src/App.js
+++ src/App.js
@@ -29,11 +29,17 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  // Toggle Reminder
+
+  const toggleReminder = (id) => {
+    console.log(id);
+  };
+
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "You have no tasks"
       )}
