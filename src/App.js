432d334e91f936d6180e8ceda36ef40392b84f8a
Victor Uland <53414263+vuduland@users.noreply.github.com>
2021-01-25 20:10:14 -0600

    add Task.js component; move tasks array to App.js level to have all info available instead of using useContext or Redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors' Appointment",
+      day: "February 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "February 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Grocery shopping",
+      day: "February 5th at 4:30pm",
+      reminder: false,
+    },
+  ]);
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
