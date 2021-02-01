d62d8611c445d7c91f6d7b1802ea73916fbe3c93
Victor Uland <53414263+vuduland@users.noreply.github.com>
2021-02-01 04:36:28 -0600

    toggle reminder on server; fixed broken style updating

--- src/App.js
+++ src/App.js
@@ -23,6 +23,14 @@ function App() {
     return data;
   };
 
+  // Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+
+    return data;
+  };
+
   // Add task
 
   const addTask = async (task) => {
@@ -52,11 +60,24 @@ function App() {
 
   // Toggle Reminder
 
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    });
+
+    const data = await res.json();
+
     setTasks(
       tasks.map((task) =>
         task.id === id
-          ? (task.reminder = { ...task, reminder: !task.reminder })
+          ? (task.reminder = { ...task, reminder: data.reminder })
           : task
       )
     );
