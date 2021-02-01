0fa7fa9909fb8e02558abf52490990544f80e706
Victor Uland <53414263+vuduland@users.noreply.github.com>
2021-02-01 03:53:47 -0600

    added showAddTask to hide addTask when not in use; also dynamic style and text to button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -25,10 +27,12 @@ function App() {
   ]);
 
   // Add task
-  
+
   const addTask = (task) => {
-    console.log(task)
-  }
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
 
   // Delete task
 
@@ -50,8 +54,11 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
