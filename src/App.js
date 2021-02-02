b1956f7400c4ffa02602b3c44d946537faefebd7
Victor Uland <53414263+vuduland@users.noreply.github.com>
2021-02-01 23:02:05 -0600

    add About page routing using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddTask from "./components/AddTask";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 import Tasks from "./components/Tasks";
+import About from "./components/About";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
@@ -84,18 +88,26 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <Header
-        onAdd={() => setShowAddTask(!showAddTask)}
-        showAdd={showAddTask}
-      />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
-        "You have no tasks"
-      )}
-    </div>
+    <Router>
+      <div className="container">
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+        {showAddTask && <AddTask onAdd={addTask} />}
+        {tasks.length > 0 ? (
+          <Tasks
+            tasks={tasks}
+            onDelete={deleteTask}
+            onToggle={toggleReminder}
+          />
+        ) : (
+          "You have no tasks"
+        )}
+        <Route path="/about" component={About} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &#169; 2021</p>
+      <a href="/about">About</a>
+    </footer>
+  );
+};
+
+export default Footer;
